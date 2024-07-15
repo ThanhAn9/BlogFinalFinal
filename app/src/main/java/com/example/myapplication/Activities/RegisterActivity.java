ca5e9ff6602d597702abbf68e70236a424236ca4
TRẦN THÀNH AN <22dh114430@st.huflit.edu.vn>
2024-07-16 04:38:48 +0700

    3-6

--- app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/Activities/RegisterActivity.java
@@ -13,10 +13,23 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -24,13 +37,51 @@ public class RegisterActivity extends AppCompatActivity {
     static int PReqCode = 1;
     Uri pickedImgUri;
     static int REQUESCODE = 1;
+    private EditText userEmail, userPassword, userPAsswrod2, userName;
+    private ProgressBar loadingProgress;
+    private Button  regBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+
+        userEmail = findViewById(R.id.regMail);
+        userPassword = findViewById(R.id.regPassword);
+        userPAsswrod2 = findViewById(R.id.regPassword2);
+        userName = findViewById(R.id.regName);
+        loadingProgress = findViewById(R.id.regProgressBar);
+        regBtn = findViewById(R.id.regBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regBtn.setVisibility(View.INVISIBLE);
+                loadingProgress.setVisibility(View.VISIBLE);
+                final String email = userEmail.getText().toString();
+                final String password = userPassword.getText().toString();
+                final String password2 = userPAsswrod2.getText().toString();
+                final String name = userName.getText().toString();
+
+                if(email.isEmpty() || name.isEmpty() || password.isEmpty() || !password.equals(password2)){
+                    showMessage("Hãy điền đủ thông tin");
+                    regBtn.setVisibility(View.VISIBLE);
+                    loadingProgress.setVisibility(View.INVISIBLE);
+
+                }else {
+                    //Tạo tk nếu email hợp lệ
+                    CreateUserAccount (email,name,password);
+                }
+            }
+        });
+
+        loadingProgress.setVisibility(View.VISIBLE);
+
         ImgUserPhoto = findViewById(R.id.regUserPhoto);
+
 
         ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +93,83 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void CreateUserAccount(String email, String name, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            showMessage("Tạo tài khoản thành công");
+                            //Update profile sau khi tạo tài khoản
+
+                            updateUserInfo (name,pickedImgUri,mAuth.getCurrentUser());
+
+                        }else {
+                            showMessage("Tạo tài khoản thất bại"+ task.getException().getMessage());
+                            regBtn.setVisibility(View.VISIBLE);
+                            loadingProgress.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+
+    }
+
+    private void updateUserInfo(String name, Uri pickedImgUri, FirebaseUser currentUser) {
+
+        //upload user photo to firebase storage và lấy url ảnh
+        StorageReference mStorage = FirebaseStorage.getInstance().getReference().child("users_photos");
+        StorageReference imageFilePath = mStorage.child(pickedImgUri.getLastPathSegment());
+        imageFilePath.putFile(pickedImgUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                //upload ảnh thành công -> lấy url của ảnh
+                imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+
+                        UserProfileChangeRequest profileUpdate = new UserProfileChangeRequest.Builder()
+                                .setDisplayName(name)
+                                .build();
+                        currentUser.updateProfile(profileUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+                                            showMessage("Đăng ký thành công");
+                                            updateUI();
+                                        }
+                                    }
+                                });
+
+
+
+
+                    }
+                });
+
+            }
+        });
+
+
+    }
+
+    private void updateUI() {
+
+        Intent homeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(homeActivity);
+        finish();
+
+    }
+
+    //Hiển thị thông báo
+    private void showMessage(String message) {
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+        
     }
 
     private void openGallery() {
