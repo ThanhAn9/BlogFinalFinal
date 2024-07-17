a1369128f85a38887bf380209ac8af8fbaf2fbe6
Pham Thanh Thuan <thanhthuan009753@gmail.com>
2024-07-17 21:55:42 +0700

    xong #9

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -9,14 +9,69 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.myapplication.Adapters.PostAdapter;
+import com.example.myapplication.Models.Post;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentHomeBinding;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView postRecyclerView;
+    PostAdapter postAdapter;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    List<Post> postList;
+
+    public HomeFragment(){
+
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View fragmentView = inflater.inflate(R.layout.fragment_home,container,false);
+        postRecyclerView = fragmentView.findViewById(R.id.postRV);
+        postRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        firebaseDatabase =FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Posts");
+
+        return fragmentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList = new ArrayList<>();
+                for(DataSnapshot postsnap: snapshot.getChildren()){
+
+                    Post post = postsnap.getValue(Post.class);
+                    postList.add(post);
+
+                }
+
+                postAdapter = new PostAdapter(getActivity(),postList);
+                postRecyclerView.setAdapter(postAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
