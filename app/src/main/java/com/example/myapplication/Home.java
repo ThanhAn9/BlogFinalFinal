5dd984ea679396b31b43332b8cec6e1d12fb029a
TRẦN THÀNH AN <22dh114430@st.huflit.edu.vn>
2024-07-17 18:15:33 +0700

    xong #8

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -1,9 +1,15 @@
 package com.example.myapplication;
+import java.lang.String;
+import java.util.Objects;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -12,20 +18,32 @@ import android.view.Menu;
 
 import com.bumptech.glide.Glide;
 import com.example.myapplication.Activities.LoginActivity;
+import com.example.myapplication.Activities.RegisterActivity;
+import com.example.myapplication.Models.Post;
 import com.example.myapplication.R;
 import com.example.myapplication.ui.home.HomeFragment;
 import com.example.myapplication.ui.home.HomeViewModel;
 import com.example.myapplication.ui.profile.ProfileFragment;
 import com.example.myapplication.ui.settings.SettingsFragment;
 import com.example.myapplication.ui.slideshow.SlideshowFragment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -34,15 +52,19 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
 
 public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    private static final int PReqCode = 2;
+    private static final int REQUESCODE =2;
     private AppBarConfiguration mAppBarConfiguration;
     FirebaseAuth mAuth;
     FirebaseUser currentUser;
@@ -50,6 +72,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
     ImageView popupUserImage, popupPostImage, popupAddBtn;
     TextView popupTitle, popupDescription;
     ProgressBar popupClickProgress;
+    private Uri pickedImgUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +86,7 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         mAuth = FirebaseAuth.getInstance();
         currentUser=mAuth.getCurrentUser();
         iniPopup();
+        setupPopupImageClick();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -90,12 +114,82 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         updateNavHeader();
     }
 
-//Khai báo các biến dùng post
+    private void setupPopupImageClick() {
+
+        popupPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // mở thư viện ảnh
+                // kiểm tra quyền truy cập ảnh
+
+                checkAndRequestForPermission();
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PReqCode) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                openGallery();
+            } else {
+                Toast.makeText(Home.this, "Quyền bị từ chối", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void checkAndRequestForPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(Home.this, android.Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(Home.this, android.Manifest.permission.READ_MEDIA_IMAGES)) {
+                    Toast.makeText(Home.this, "Hãy cấp quyền truy cập hình ảnh", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(Home.this, new String[]{android.Manifest.permission.READ_MEDIA_IMAGES}, PReqCode);
+                }
+            } else {
+                openGallery();
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(Home.this, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(Home.this, android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(Home.this, "Hãy cấp quyền", Toast.LENGTH_SHORT).show();
+                } else {
+                    ActivityCompat.requestPermissions(Home.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PReqCode);
+                }
+            } else {
+                openGallery();
+            }
+        }
+    }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, REQUESCODE);
+    }
+
+    //Người dùng chọn ảnh
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUESCODE && data != null) {
+            pickedImgUri = data.getData();
+            popupPostImage.setImageURI(pickedImgUri);
+
+        }
+    }
+
+    //Khai báo các biến dùng post
     private void iniPopup(){
 
         popAddPost = new Dialog(this);
         popAddPost.setContentView(R.layout.popup_add_post);
-        popAddPost.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Objects.requireNonNull(popAddPost.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         popAddPost.getWindow().setLayout(Toolbar.LayoutParams.MATCH_PARENT,Toolbar.LayoutParams.WRAP_CONTENT);
         popAddPost.getWindow().getAttributes().gravity = Gravity.TOP;
 
@@ -106,10 +200,9 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
         popupAddBtn = popAddPost.findViewById(R.id.popup_add);
         popupClickProgress = popAddPost.findViewById(R.id.popup_progressBar);
 
-        //Tải ảnh đại diện người dùng
+        //Tải ảnh đại diện người dùng, ko load đc
 
         Glide.with(Home.this).load(currentUser.getPhotoUrl()).into(popupUserImage);
-
 
         //xử lý khi ấn vào post
         popupAddBtn.setOnClickListener(new View.OnClickListener() {
@@ -118,13 +211,94 @@ public class Home extends AppCompatActivity implements NavigationView.OnNavigati
                 popupAddBtn.setVisibility(View.INVISIBLE);
                 popupClickProgress.setVisibility(View.VISIBLE);
 
+                //Thêm điều kiện cho Title và description và post image
+                if (!popupTitle.getText().toString().isEmpty()
+                        && !popupDescription.getText().toString().isEmpty()
+                        && pickedImgUri != null ) {
+                    //nếu không rỗng -> Tạo post và thêm vào database
+                    //Upload ảnh vào Database
+                    //kết nối tới firebase storage
+                    StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("blog_images");
+                    final StorageReference imageFilePath = storageReference.child(pickedImgUri.getLastPathSegment());
+
+                    imageFilePath.putFile(pickedImgUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            imageFilePath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    if (uri != null) {
+                                        String imageDownloadLink = uri.toString();
+
+                                        // Tạo đối tượng Post
+                                        Post post = new Post(popupTitle.getText().toString(),
+                                                popupDescription.getText().toString(),
+                                                imageDownloadLink,
+                                                currentUser.getUid(),
+                                                currentUser.getPhotoUrl() != null ? currentUser.getPhotoUrl().toString() : "");
+
+                                        // Thêm vào cơ sở dữ liệu
+                                        addPost(post);
+                                    } else {
+                                        // Xử lý trường hợp URI là null
+                                        showMessage("Không thể lấy URL ảnh");
+                                        popupClickProgress.setVisibility(View.INVISIBLE);
+                                        popupAddBtn.setVisibility(View.VISIBLE);
+                                    }
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    // Xử lý khi upload thất bại
+                                    showMessage("Upload thất bại");
+                                    popupClickProgress.setVisibility(View.INVISIBLE);
+                                    popupAddBtn.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }
+                    });
 
 
 
+                }else {
+                    showMessage("Không để trống hình, Title, Description");
+                    popupAddBtn.setVisibility(View.VISIBLE);
+                    popupClickProgress.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
+
     }
+
+    private void addPost(Post post) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Posts").push();
+
+        // lấy id post và cập nhật id
+        String key = myRef.getKey();
+        post.setPostKey(key);
+
+
+        // thêm post vào database
+        myRef.setValue(post).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                showMessage("Thêm bài thành công");
+                popupClickProgress.setVisibility(View.INVISIBLE);
+                popupAddBtn.setVisibility(View.VISIBLE);
+                popAddPost.dismiss();
+            }
+        });
+    }
+
+    private void showMessage(String message) {
+
+        Toast.makeText(Home.this, message   , Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
