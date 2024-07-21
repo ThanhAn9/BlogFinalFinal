a2a0802badeaf0ecc91c73eeb1e28f9606664cc8
Pham Thanh Thuan <thanhthuan009753@gmail.com>
2024-07-21 22:29:25 +0700

    xong #10-11

---
+++ app/src/main/java/com/example/myapplication/Activities/PostDetailActivity.java
@@ -0,0 +1,99 @@
+package com.example.myapplication.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.myapplication.Adapters.CommentAdapter;
+import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Comment;
+
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+
+public class PostDetailActivity extends AppCompatActivity {
+
+    ImageView imgPost,imgUserPost,imgCurrentUser;
+    TextView txtPostDesc,txtPostDateName,txtPostTitle;
+    EditText editTextComment;
+    Button btnAddComment;
+    String PostKey;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseDatabase firebaseDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_detail);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        //getSupportActionBar().hide();
+
+        // ini Views
+        //RvComment = findViewById(R.id.rv_comment);
+        imgPost =findViewById(R.id.post_detail_img);
+        imgUserPost = findViewById(R.id.post_detail_user_img);
+        imgCurrentUser = findViewById(R.id.post_detail_currentuser_img);
+
+        txtPostTitle = findViewById(R.id.post_detail_title);
+        txtPostDesc = findViewById(R.id.post_detail_desc);
+        txtPostDateName = findViewById(R.id.post_detail_date_name);
+
+        editTextComment = findViewById(R.id.post_detail_comment);
+        btnAddComment = findViewById(R.id.post_detail_add_comment_btn);
+
+        String postImage = getIntent().getExtras().getString("postImage");
+        Glide.with(this).load(postImage).into(imgPost);
+
+        String postTitle = getIntent().getExtras().getString("title");
+        txtPostTitle.setText(postTitle);
+
+        String userpostImage = getIntent().getExtras().getString("userPhoto");
+        Glide.with(this).load(userpostImage).into(imgUserPost);
+
+        String postDescription = getIntent().getExtras().getString("description");
+        txtPostDesc.setText(postDescription);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        Glide.with(this).load(firebaseUser.getPhotoUrl()).into(imgCurrentUser);
+
+        PostKey = getIntent().getExtras().getString("postKey");
+
+        String date = timestampToString(getIntent().getExtras().getLong("postDate"));
+        txtPostDateName.setText(date);
+    }
+
+    private String timestampToString(long time) {
+
+        Calendar calendar = Calendar.getInstance(Locale.ENGLISH);
+        calendar.setTimeInMillis(time);
+        String date = DateFormat.format("dd-MM-yyyy",calendar).toString();
+        return date;
+
+
+    }
+}
